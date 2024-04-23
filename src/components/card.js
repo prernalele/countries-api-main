160af67408d48a94f26b40d147416612038e9714
Prerna Lele <prernalele@gmail.com>
2024-04-23 08:45:15 -0400

    feat: adds flag and styling

--- src/components/card.js
+++ src/components/card.js
@@ -3,23 +3,28 @@ import "./Card.css";
 
 const Card = ({ data }) => {
   return data.map((singleDataPoint, index) => {
+    console.log("singleDataPoint", singleDataPoint);
+    const { flags, name, capital, population, region } = singleDataPoint;
     return (
       <div key={index} className="wrapper">
-        <div className="name">
-          <span className="label">Name</span>
-          <span className=" name-value"> {singleDataPoint.name}</span>
-        </div>
-        <div className="capital">
-          <span className="label">Capital </span>
-          <span className="capital-value"> {singleDataPoint.capital}</span>
-        </div>
-        <div className="region">
-          <span className="label">Region</span>
-          <span className="region-value">{singleDataPoint.region}</span>
-        </div>
-        <div className="population">
-          <span className="label">Population</span>
-          <span className="population-value">{singleDataPoint.population}</span>
+        <img alt={`flag of ${name}`} src={flags.svg} />
+        <div className="cardDetailsWrapper">
+          <div className="name">
+            <span className="label">Name:</span>
+            <span className="name-value"> {name}</span>
+          </div>
+          <div className="capital">
+            <span className="label">Capital: </span>
+            <span className="capital-value"> {capital}</span>
+          </div>
+          <div className="region">
+            <span className="label">Region:</span>
+            <span className="region-value">{region}</span>
+          </div>
+          <div className="population">
+            <span className="label">Population:</span>
+            <span className="population-value">{population}</span>
+          </div>
         </div>
       </div>
     );
