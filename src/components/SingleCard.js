df7ec40f4010df20b5cbd5fafc649ade905d3863
Prerna Lele <prernalele@gmail.com>
2024-09-29 18:00:05 -0400

    feat: adds logic for border countries

--- src/components/SingleCard.js
+++ src/components/SingleCard.js
@@ -1,9 +1,11 @@
 import React, {useContext} from 'react'
 import ThemeSwitchContext from '../context/ThemeSwitchContext'
+import DataContext from '../dataContext'
 import './singleCard.css'
 
 const SingleCard = ({clickedCountry, isSingleCardClicked, setIsCardClicked}) => {
     const { theme } = useContext(ThemeSwitchContext)
+    const {allData } = useContext(DataContext)
     const { flag
             ,name
             ,nativeName
@@ -13,32 +15,45 @@ const SingleCard = ({clickedCountry, isSingleCardClicked, setIsCardClicked}) => 
             ,capital
             ,topLevelDomain
             ,currencies
-            ,languages} = clickedCountry
+            ,languages
+            , borders} = clickedCountry
+
     const backToHome = () => {
         setIsCardClicked(!isSingleCardClicked)
     }
-    debugger
+
+    const borderFullNames = 
+    borders.map((border) => allData.find((eachCountry) => eachCountry.alpha3Code === border ).name) 
+
     return (
         <div className={`containerSingle ${theme}Details`}>
             <button className={`backButton ${theme}Details`} onClick={backToHome}>Back</button>
             <div className='details'>
                 <img className="leftSection flag" alt={`flag of ${name}`} src={flag} />
-                <div className='middleSection'>
-                    <h3>{name} </h3>
-                    <div className='otherDetails'>
-                        <div>Native Name: {nativeName}</div>
-                        <div>Population: {population}</div>
-                        <div>Region: {region}</div>
-                        <div>Sub Region: {subregion}</div>
-                        <div>Capital: {capital || ''}</div>
-                    </div>
+                <div>
+                    <div className='middleSection'>
+                        <h3>{name} </h3>
+                        <div className='otherDetails'>
+                            <div>Native Name: {nativeName}</div>
+                            <div>Population: {population}</div>
+                            <div>Region: {region}</div>
+                            <div>Sub Region: {subregion}</div>
+                            <div>Capital: {capital || ''}</div>
+                        </div>
 
+                    </div>
+                    <div className='rightSection'>
+                        <div>Top Level Domain: {topLevelDomain || topLevelDomain.map((domain) => <span>{domain}</span>)}</div>
+                        <div>Currencies: {''||currencies?.map((currency) => <span>{currency.name}</span>)}</div>
+                        <div>Languages: {languages.map((lang) => <span>{lang.name}</span> )}</div>
+                    </div>
+                    <div className='borders'>
+                        {borderFullNames.map((code) => 
+                        <button>{code}</button>
+                        )}
+                    </div>
                 </div>
-                <div className='rightSection'>
-                    <div>Top Level Domain: {topLevelDomain || topLevelDomain.map((domain) => <span>{domain}</span>)}</div>
-                    <div>Currencies: {''||currencies?.map((currency) => <span>{currency.name}</span>)}</div>
-                    <div>Languages: {languages.map((lang) => <span>{lang.name}</span> )}</div>
-                </div>
+
             </div>
         </div>
     )
